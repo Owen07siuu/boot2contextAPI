305ee11b315f80df4f94d281f75bdd6ef73bac86
Owen07 <owqn2005@gmail.com>
2024-09-13 06:39:27 -0500

    form validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../context/Context';
 
 export const Login = () => {
 
   //Extraer user y setuser del contexto usando usecontext
   const { user, setUser } = useContext(Context);
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  //metodo para validar username y que no permita espacios y mayusculas
+  const handleUsernameV = e => {
+    const value = e.target.value.replace(/\s+/g, '').toLowerCase();
+    setUsername(value);
+  };
+
+  //metodo para validar el nombre y cambiar primera letra en mayuscula
+  const handleNameV = e =>{
+  const value = e.target.value
+  .split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+  .join(' ');
+  setName(value);
+  }
+
+  //metodo para validar el numero
+  const handlePhoneV = e => {
+    const value = e.target.value.replace(/[^0-9]/g, '');
+    setPhone(value);
+  }
+
   // Metodo que se ejecuta al hacer clic en enviar el formulario
   const saveData = e => {
     e.preventDefault();
@@ -24,9 +49,9 @@ export const Login = () => {
     <div>
       <h1 className='text-center'>Login</h1>
       <form className='login' onSubmit={saveData}>
-        <input type="text" name='username' placeholder='username'/>
-        <input type="text" name='name' placeholder='name'/>
-        <input type="text" name='phone' placeholder='phone'/>
+        <input type="text" name='username' placeholder='username' value={username} onChange={handleUsernameV} required  />
+        <input type="text" name='name' placeholder='name' value={name} onChange={handleNameV} required />
+        <input type="text" name='phone' placeholder='phone' value={phone} onChange={handlePhoneV} required />
         <input type="submit" value="enviar" />
       </form>
     </div>
